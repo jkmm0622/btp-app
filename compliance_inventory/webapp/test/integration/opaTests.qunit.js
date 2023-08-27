c27146c9a939b15caa73a8c9df6617c297740e07
Jitendra Kumar <jitendra.kumar@maventic.com>
2023-08-27 03:33:55 +0000

    initial commit

---
+++ compliance_inventory/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/maventic/complianceinventory/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ compliance_inventory/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"commaventic/compliance_inventory/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
